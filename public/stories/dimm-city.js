a91e8626b07a46e049dbdd29e5fc9a692f006640
itlackey <itlackey@outlook.com>
2022-02-08 00:17:54 -0600

    added background & cachevideo macros & moved html

--- public/stories/dimm-city.js
+++ public/stories/dimm-city.js
@@ -11,15 +11,31 @@
         });
     }
 
+    if (!SugarCube.Macro.has("cachevideo")) {
+        SugarCube.Macro.add("cachevideo", {
+            handler() {
+                const el = document.createElement("video");
+                el.toggleAttribute("muted");
+                el.toggleAttribute("disablePictureInPicture");
+                el.toggleAttribute("playsinline");
+                el.toggleAttribute("hidden");
+                el.toggleAttribute("preload");
+                el.setAttribute("poster", this.args.length >= 2 ? this.args[1] : "");
+                el.innerHTML = `<source src="${this.args[0]}" type="video/mp4" />`;
+
+                this.output.append(el);
+            },
+        });
+    }
+
     if (!SugarCube.Macro.has("background")) {
         SugarCube.Macro.add("background", {
-            tags: null,
-            handler: function () {
+            handler() {
                 /* <video class="background" autoplay loop muted disablePictureInPicture playsinline poster="/stories/intro/index.png">
                     <source src="https://files.dimm.city/stories/intro/index.mp4" type="video/mp4"> 
                 </video>*/
                 const el = document.createElement("video");
-                
+
                 el.toggleAttribute("autoplay");
                 el.toggleAttribute("loop");
                 el.toggleAttribute("muted");
@@ -27,7 +43,7 @@
                 el.toggleAttribute("playsinline");
                 el.classList.add("background");
                 el.setAttribute("poster", this.args.length >= 2 ? this.args[1] : "");
-                el.append(`<source src="${this.args[0]}" type="video/mp4" />`);
+                el.innerHTML = `<source src="${this.args[0]}" type="video/mp4" />`;
 
                 this.output.append(el);
             },
