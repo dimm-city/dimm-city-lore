5567ed34f2061da2e2974b7b8239301c0d568b87
itlackey <itlackey@outlook.com>
2022-02-08 21:58:04 -0600

    sporos page stubbed

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,9 +1,9 @@
 document.addEventListener("story.selected", (e: CustomEvent) => {
     console.log("story.selected", e);
     if (e.detail > "" && e.detail != "stories/none.html") {
-        document.querySelector(".vertical-acordion").classList.remove("bottom");
+        document.querySelector("body").classList.remove("bottom");
     } else {
-        document.querySelector(".vertical-acordion").classList.add("bottom");
+        document.querySelector("body").classList.add("bottom");
     }
 });
 
